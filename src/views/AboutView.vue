c37a0614254f0a724b12585d2d3f4551da7f9eab
yardaandika <myardaandika304@gmail.com>
2023-05-20 18:03:16 +0700

    updatevue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,46 @@
+<template>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Biodata</title>
+</head>
+<body align="center">
+  <h1>Biodata</h1>
+  <table>
+    <tr>
+      <th>Nama</th>
+      <td>Mardea Oksana Apriani</td>
+    </tr>
+    <tr>
+      <th>Tanggal Lahir</th>
+      <td>6 Maret 2003</td>
+    </tr>
+    <tr>
+      <th>Alamat</th>
+      <td>Pondokan Putri Maharatu, Jl. Kaharudin Nst, Pekanbaru</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>mardeaoksanaapriani@student.uir.ac.id</td>
+    </tr>
+    <tr>
+      <th>No. Telepon</th>
+      <td>085173019790</td>
+    </tr>
+  </table>
+</body>
+</html>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
